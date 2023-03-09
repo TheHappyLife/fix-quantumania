69840152e8dbe480f33ceceac9153441fe74905c
TheHappyLife <dangnguyen120699@gmail.com>
2023-03-09 15:08:45 +0700

    f10

--- lib/components/account_page_new/account_page_new_widget.dart
+++ lib/components/account_page_new/account_page_new_widget.dart
@@ -268,63 +268,63 @@ class _AccountPageNewWidgetState extends State<AccountPageNewWidget>
                     ),
                   ),
                 ),
-                // InkWell(
-                //   onTap: () async {
-                //     await showModalBottomSheet(
-                //       isScrollControlled: true,
-                //       backgroundColor: Color(0x4D000000),
-                //       barrierColor: Color(0x4D000000),
-                //       context: context,
-                //       builder: (context) {
-                //         return Padding(
-                //           padding: MediaQuery.of(context).viewInsets,
-                //           child: Container(
-                //             height: MediaQuery.of(context).size.height * 1.0,
-                //             child: LogOutConfirmWidget(),
-                //           ),
-                //         );
-                //       },
-                //     ).then((value) => setState(() {}));
+                InkWell(
+                  onTap: () async {
+                    await showModalBottomSheet(
+                      isScrollControlled: true,
+                      backgroundColor: Color(0x4D000000),
+                      barrierColor: Color(0x4D000000),
+                      context: context,
+                      builder: (context) {
+                        return Padding(
+                          padding: MediaQuery.of(context).viewInsets,
+                          child: Container(
+                            height: MediaQuery.of(context).size.height * 1.0,
+                            child: LogOutConfirmWidget(),
+                          ),
+                        );
+                      },
+                    ).then((value) => setState(() {}));
 
-                //     await lottieLogoutController.forward();
-                //     lottieLogoutController.reset();
-                //   },
-                //   child: Container(
-                //     width: MediaQuery.of(context).size.width * 1.0,
-                //     decoration: BoxDecoration(
-                //       color: Color(0x00FFFFFF),
-                //     ),
-                //     child: Padding(
-                //       padding: EdgeInsetsDirectional.fromSTEB(
-                //           24.0, 14.0, 24.0, 14.0),
-                //       child: Row(
-                //         mainAxisSize: MainAxisSize.max,
-                //         mainAxisAlignment: MainAxisAlignment.start,
-                //         children: [
-                //           Lottie.asset(
-                //             'assets/lottie_animations/2129-login-outline-edited.json',
-                //             width: 36.0,
-                //             height: 36.0,
-                //             fit: BoxFit.cover,
-                //             controller: lottieLogoutController,
-                //             onLoaded: (composition) => lottieLogoutController
-                //                 .duration = composition.duration,
-                //           ),
-                //           Expanded(
-                //             child: wrapWithModel(
-                //               model: _model.menuItemTextModel2,
-                //               updateCallback: () => setState(() {}),
-                //               child: MenuItemTextWidget(
-                //                 title: 'Log Out',
-                //                 subTitle: '',
-                //               ),
-                //             ),
-                //           ),
-                //         ],
-                //       ),
-                //     ),
-                //   ),
-                // ),
+                    await lottieLogoutController.forward();
+                    lottieLogoutController.reset();
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 1.0,
+                    decoration: BoxDecoration(
+                      color: Color(0x00FFFFFF),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(
+                          24.0, 14.0, 24.0, 14.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          // Lottie.asset(
+                          //   'assets/lottie_animations/2129-login-outline-edited.json',
+                          //   width: 36.0,
+                          //   height: 36.0,
+                          //   fit: BoxFit.cover,
+                          //   controller: lottieLogoutController,
+                          //   onLoaded: (composition) => lottieLogoutController
+                          //       .duration = composition.duration,
+                          // ),
+                          Expanded(
+                            child: wrapWithModel(
+                              model: _model.menuItemTextModel2,
+                              updateCallback: () => setState(() {}),
+                              child: MenuItemTextWidget(
+                                title: 'Log Out',
+                                subTitle: '',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
